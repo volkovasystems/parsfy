a104719683d87901235ffecc5697650cd1c63087
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-27 19:57:55 +0800

    Updated test.

--- parsfy-test.js
+++ parsfy-test.js
@@ -8,12 +8,6 @@ assert.equal( parsfy( 1 ), 1, "should be equal to 1" );
 
 assert.equal( parsfy( false ), false, "should be equal to false" );
 
-/*	@fixme
-	reviver value is always undefined and has error "Unexpected token h in JSON at position 0"
-assert.equal( parsfy( "hello" ), "hello", "should be equal" );
-	@end-fixme
-*/
-
 assert.deepEqual( parsfy( JSON.stringify( { "hello": "world" } ) ),
 	{ "hello": "world" }, "should be equal to { 'hello': 'world' }" );
 
